9a0a411a637c1f64b070f53980a06ab00e8ea77b
Peter Kim <peter@apollographql.com>
2019-11-03 17:13:57 -0500

    fix gql query; update logstash confg

--- k6_script.js
+++ k6_script.js
@@ -1,16 +1,20 @@
 import http from "k6/http";
 
 export let options = {
-  vus: 10,
-  duration: "30s"
+  vus: 100,
+  duration: "60s"
 };
 
 export default function() {
   var random = Math.floor(Math.random() * 100) + 1;
-  var resta_params =  { headers: { "Host": "rest-a.com" } }
-  var restb_params =  { headers: { "Host": "rest-b.com" } }
-  var graphqla_params =  { headers: { "Host": "graphql-a.com" } }
+  var resta_params = { headers: { Host: "rest-a.com" } };
+  var restb_params = { headers: { Host: "rest-b.com" } };
+  var graphqla_params = { headers: { "Host": "graphql-a.com" , "Content-Type": "application/json"} };
+  var query = `
+ query hello {
+    helloGraphql
+ }`;
   http.get(`http://localhost:8000/${random}`, resta_params);
   http.get(`http://localhost:8000/${random}`, restb_params);
-  http.post(`http://localhost:8000/`)
+  http.post(`http://localhost:8000/`, JSON.stringify({query: query}), graphqla_params);
 }
